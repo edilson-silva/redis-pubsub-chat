2ea135f25d2d8e4bac2ef6ef30c87fbe177bbe0a
Edilson Silva <eddysilva1994@gmail.com>
2021-12-28 11:58:49 -0300

    added resource to join in a chat

--- connection/redis_connection.py
+++ connection/redis_connection.py
@@ -1,12 +1,19 @@
+import threading
 from redis import Redis
 from exception.invalid_chat_username_exception import InvalidChatUsername
+from exception.invalid_chat_name_exception import InvalidChatName
+from types import FunctionType
 
 class Connection:
-
+    
+    __PUBLIC_CHAT_WITH_ALL_USERS = 'all'
+    
     def __init__(self, host: str = 'localhost', port: int = 6379) -> None:
         self.__users_list_control = 'users'
         self.__connection = Redis(host=host, port=port)
-        print(f'Redis connected on port {port}')
+        self.__pubsub_client = self.__connection.pubsub()
+        self.__my_chats = []
+        print(f'Redis connected on port {port}\n')
 
     def is_available_chat_username(self, username: str) -> bool:
         """Verify if username is available to join in the chat
@@ -22,3 +29,15 @@ class Connection:
         
         index = self.__connection.lpos(self.__users_list_control, username)
         return index is None
+
+    def join_in_chat(self, on_new_message_callback: FunctionType, on_failure_callback: FunctionType, name: str = __PUBLIC_CHAT_WITH_ALL_USERS):
+        """Join in specific chat based on chat name
+
+        Args:
+            on_new_message_callback (FunctionType): A function to hanle new messages that will be receive an unique dict parameter as a message
+            on_failure_callback (FunctionType): [description]
+            name (str, optional): The chat name to join. Defaults to __PUBLIC_CHAT_WITH_ALL_USERS.
+        """
+        self.__pubsub_client.psubscribe(**{name: on_new_message_callback})
+        chat_thread = self.__pubsub_client.run_in_thread(sleep_time=0.001, daemon=True, exception_handler=on_failure_callback)
+        self.__my_chats.append(chat_thread)
--- index.py
+++ index.py
@@ -1,4 +1,6 @@
+from threading import Thread
 from connection.redis_connection import Connection
+from typing import Any
 
 connection = Connection()
 
@@ -15,3 +17,20 @@ while True:
         break
     
     print(f'Username {my_user_name} is unavailable, please try again\n')
+
+
+def on_failure(_: Any, redis_pubsub: Any, chat_thread: Thread):
+    """Manipulate exceptions on receive message process
+
+    Args:
+        _ (str): An empty value (iscard value)
+        redis_pubsub (Any): The PubSub connection object
+        chat_thread (Thread): The chat thread object
+    """
+    pass    
+
+def on_new_message(message):
+    print(message)
+
+chat_name = input('\nEnter the chat name: ').strip().lower()
+connection.join_in_chat(on_new_message_callback=on_new_message, on_failure_callback=on_failure, name=chat_name)
