d3d17a694840f1d88743e7b9bc1dcc58742ab8a9
Edilson Silva <eddysilva1994@gmail.com>
2021-12-28 11:21:43 -0300

    added username verification to prevent double users with same name on chat

--- connection/redis_connection.py
+++ connection/redis_connection.py
@@ -1,7 +1,24 @@
 from redis import Redis
+from exception.invalid_chat_username_exception import InvalidChatUsername
 
 class Connection:
-    
+
     def __init__(self, host: str = 'localhost', port: int = 6379) -> None:
+        self.__users_list_control = 'users'
         self.__connection = Redis(host=host, port=port)
         print(f'Redis connected on port {port}')
+
+    def is_available_chat_username(self, username: str) -> bool:
+        """Verify if username is available to join in the chat
+
+        Args:
+            username (str): Username to verify
+
+        Returns:
+            bool: True if available and false otherwise
+        """
+        if not username.strip():
+            raise InvalidChatUsername('Please, send username')
+        
+        index = self.__connection.lpos(self.__users_list_control, username)
+        return index is None
--- index.py
+++ index.py
@@ -1,3 +1,17 @@
 from connection.redis_connection import Connection
 
 connection = Connection()
+
+my_user_name = ''
+
+
+while True:
+    # Getting chat user name and generate your unique identifier
+    my_user_name = input('Please, enter your name: ').strip().lower()
+
+    is_available = connection.is_available_chat_username(my_user_name)
+    
+    if is_available:
+        break
+    
+    print(f'Username {my_user_name} is unavailable, please try again\n')
