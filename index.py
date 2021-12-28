52d36beb7b43714fe40022832408e4021b9ca309
Edilson Silva <eddysilva1994@gmail.com>
2021-12-28 12:54:06 -0300

    added handling for new messages displaying only messages from other users

--- index.py
+++ index.py
@@ -1,22 +1,24 @@
+import json
+
 from threading import Thread
 from connection.redis_connection import Connection
 from typing import Any
 
 connection = Connection()
 
-my_user_name = ''
+my_username = ''
 
 
 while True:
     # Getting chat user name and generate your unique identifier
-    my_user_name = input('Please, enter your name: ').strip().lower()
+    my_username = input('Please, enter your name: ').strip().lower()
 
-    is_available = connection.is_available_chat_username(my_user_name)
+    is_available = connection.is_available_chat_username(my_username)
     
     if is_available:
         break
     
-    print(f'Username {my_user_name} is unavailable, please try again\n')
+    print(f'Username {my_username} is unavailable, please try again\n')
 
 
 def on_failure(_: Any, redis_pubsub: Any, chat_thread: Thread):
@@ -29,8 +31,28 @@ def on_failure(_: Any, redis_pubsub: Any, chat_thread: Thread):
     """
     pass    
 
-def on_new_message(message):
-    print(message)
+def on_new_message(message: dict):
+    """Receive a new message
+
+    Args:
+        message (dict): New received message
+    """
+    if message:
+        message_type = message['type']
+        
+        if message_type == 'pmessage':
+            message = message['data'].decode('utf-8')  # Retrieve string from byte array
+            message = json.loads(message)  # Parse string to dict
+            
+            username = message.get('username', '')
+            message = message.get('message', '').strip()
+            
+            if message and username and username != my_username:
+                print(f'\n{username.upper()}: {message}')
+                raise Exception
+        else:
+            print(f'Event: {message_type}') 
+
 
 chat_name = input('\nEnter the chat name: ').strip().lower()
 connection.join_in_chat(on_new_message_callback=on_new_message, on_failure_callback=on_failure, chat_name=chat_name)
@@ -39,4 +61,4 @@ connection.join_in_chat(on_new_message_callback=on_new_message, on_failure_callb
 # Getting user message to publish
 while True:
     message = input('Please, enter a message: ')
-    connection.send_message_to_chat(username=my_user_name, chat_name=chat_name, message=message)
+    connection.send_message_to_chat(username=my_username, chat_name=chat_name, message=message)
